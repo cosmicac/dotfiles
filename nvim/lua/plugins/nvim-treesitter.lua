5f765e600e5d9194432b94bf15126a5ea7251e83
Tom Li <weirdotomli@gmail.com>
2024-03-06 20:48:47 -0800

    Update treesitter config.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -10,6 +10,7 @@ return {
       --   :TSInstall {parser}
       --
       require'nvim-treesitter.configs'.setup {
+        ensure_installed = { "lua", "python" },
         highlight = {
           enable = true,
         },
