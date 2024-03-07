2196387922fe20e5d54b0e37f1afbcba7257a40f
Tom Li <weirdotomli@gmail.com>
2024-03-06 20:16:06 -0800

    Treesitter configuration.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,4 +3,25 @@ return {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
+    config = function()
+      -- See ~/.local/share/nvim/lazy/nvim-treesitter/parser for a list of installed parsers.
+      -- To install a new parser:
+      --
+      --   :TSInstall {parser}
+      --
+      require'nvim-treesitter.configs'.setup {
+        highlight = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      }
+    end,
 }
